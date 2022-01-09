2df8e9a4f59808e789f95f06dbc2e2f5288d7487
Kamalakannan J <sincerekamal@gmail.com>
2022-01-10 00:40:28 +0530

    Updates

---
+++ precache-manifest.5d0b217221202e50f15b04d3c792fe5f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf50c7baea6de78ee7066ecfd598e6f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "405d77bed7687bfdaf83",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "4f86de8502822817a581",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "9cedf7cbf432eae3bbb0",
+    "url": "/static/css/main.66262938.chunk.css"
+  },
+  {
+    "revision": "a27c6494b76bfb834e4b",
+    "url": "/static/js/2.c30130b1.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.c30130b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405d77bed7687bfdaf83",
+    "url": "/static/js/3.cf9c83e3.chunk.js"
+  },
+  {
+    "revision": "4f86de8502822817a581",
+    "url": "/static/js/4.651aed64.chunk.js"
+  },
+  {
+    "revision": "9cedf7cbf432eae3bbb0",
+    "url": "/static/js/main.2a70147c.chunk.js"
+  },
+  {
+    "revision": "a198b8b1b71994b561c0",
+    "url": "/static/js/runtime-main.024ed7c7.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "67496fe3e60ce037b0447668384031eb",
+    "url": "/static/media/anna-univ-logo.67496fe3.png"
+  },
+  {
+    "revision": "c7401f520e568a2994f0269e6c537f1d",
+    "url": "/static/media/aws-logo.c7401f52.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "49303f75789e286ede69227903154c03",
+    "url": "/static/media/ge-2-logo.49303f75.jpg"
+  },
+  {
+    "revision": "fa06a7cbace5408cb3e458dc5de28776",
+    "url": "/static/media/lineage-logo.fa06a7cb.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
